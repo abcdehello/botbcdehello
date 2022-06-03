038454c0753f0c38bc424799ddffa7e9cc3c8144
abcdehello <38322485+abcdehello@users.noreply.github.com>
2022-06-04 02:00:21 +0800

    changed prefix, added mute system

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 import os
+import time
 
 import embedbuilder as builder
 import discordio as io
@@ -14,11 +15,13 @@ import hypixel as hyp
 import minecraftinfo as mcinfo
 import minesweeper as mine
 import wikipedia as wiki
+import mute as mu
 
 TOKEN=os.getenv('token')
-OWNER=str(os.getenv('owner_id'))
+OWNER=os.getenv('owner_id')
+PREFIX=os.getenv('prefix')
 
-bot=commands.Bot(command_prefix='&',intents=discord.Intents.all())
+bot=commands.Bot(command_prefix=PREFIX,intents=discord.Intents.all())
 bot.remove_command('help')
 
 @bot.event
@@ -28,12 +31,23 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
+  muted=await json.read('muted.json')
+  if not (msg.content.startswith(PREFIX)):
+    return
+  try:
+    untilunmute=muted[str(msg.author.id)]
+    if (untilunmute>=time.time()):
+      await io.reply(msg,'',await builder.buildDesc('Muted','You are muted until <t:'+str(untilunmute)+':f>',2))
+      return
+  except:
+    dummy=0
   await bot.process_commands(msg)
+  
 
 @bot.event
 async def on_command_error(ctx,error):
   if isinstance(error,commands.CommandNotFound):
-    await io.reply(ctx,'',await builder.buildDesc('Command Not Found','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Command Not Found','Please use `^help`',0))
 
 #Admin Commands
 
@@ -44,6 +58,19 @@ async def halt(ctx):
     return
   await io.send(ctx,'',await builder.buildDesc('Command Received','Bot stopping...',1))
   exit()
+
+@bot.command()
+async def mute(ctx,*args):
+  if (str(ctx.author.id)!=OWNER):
+    await io.reply(ctx,'',await builder.buildDesc('Insufficient Permission','You cannot execute this command',0))
+    return
+  try:
+    user=int(args[0])
+    dura=int(args[1])
+  except:
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
+    return
+  await mu.mute(ctx,str(user),dura)
 
 #Help Commands
 
@@ -73,7 +100,8 @@ async def spam(ctx,*args):
         msg+=' '
       msg+=str(args[i])
   except:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
+    return
   hasping=False
   if hasping:
     await io.reply(ctx,'',await builder.buildDesc('Command Blocked','Ping detected',0))
@@ -89,20 +117,20 @@ async def userinfo(ctx,*args):
   try:
     action=str(args[0])
   except:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
     return
   if (action=='namehistory') or (action=='namehis'):
     try:
       username=str(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await mcinfo.namehistory(ctx,username)
   elif (action=='skin'):
     try:
       username=str(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await mcinfo.getskin(ctx,username)
 
@@ -114,7 +142,7 @@ async def wikipedia(ctx,*args):
       title+=' '
     title+=str(args[i])
   if (title==''):
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
     return
   await wiki.search(ctx,title)
 
@@ -123,7 +151,7 @@ async def hypixel(ctx,*args):
   try:
     action=str(args[0])
   except:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
     return
   if (action=='keycheck'):
     await hyp.keyCheck(ctx)
@@ -131,7 +159,7 @@ async def hypixel(ctx,*args):
     try:
       username=str(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await hyp.statusCheck(ctx,username)
   
@@ -142,13 +170,13 @@ async def minesweeper(ctx,*args):
   try:
     action=str(args[0])
   except:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
     return
   if (action=='new'):
     try:
       diff=str(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (diff=='easy'):
       await mine.makeBoard(str(ctx.author.id),5,4)
@@ -163,7 +191,7 @@ async def minesweeper(ctx,*args):
       await mine.makeBoard(str(ctx.author.id),14,36)
       await mine.printBoard(ctx,str(ctx.author.id))
     else:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
   elif (action=='print'):
     await mine.printBoard(ctx,str(ctx.author.id))
   elif (action=='open'):
@@ -171,7 +199,7 @@ async def minesweeper(ctx,*args):
       x=int(args[1])
       y=int(args[2])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (await mine.openCell(ctx,str(ctx.author.id),x,y)):
       await mine.printBoard(ctx,str(ctx.author.id))
@@ -180,7 +208,7 @@ async def minesweeper(ctx,*args):
       x=int(args[1])
       y=int(args[2])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (await mine.flagCell(ctx,str(ctx.author.id),x,y)):
       await mine.printBoard(ctx,str(ctx.author.id))
@@ -189,21 +217,21 @@ async def minesweeper(ctx,*args):
       x=int(args[1])
       y=int(args[2])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (await mine.unflagCell(ctx,str(ctx.author.id),x,y)):
       await mine.printBoard(ctx,str(ctx.author.id))
   elif (action=='difficulty') or (action=='diff'):
     await mine.listdiff(ctx)
   else:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
 
 @bot.command(aliases=['cm'])
 async def cryptomine(ctx,*args):
   try:
     action=str(args[0])
   except:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
     return
   if (action=='new'):
     await miner.createprofile(ctx,str(ctx.author.id))
@@ -215,7 +243,7 @@ async def cryptomine(ctx,*args):
     try:
       mine=str(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await miner.buymine(ctx,str(ctx.author.id),mine)
   elif (action=='listmine') or (action=='lm'):
@@ -224,10 +252,10 @@ async def cryptomine(ctx,*args):
     try:
       id=int(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (id<1):
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await miner.sellmine(ctx,str(ctx.author.id),id)
   elif (action=='harvest'):
@@ -239,23 +267,23 @@ async def cryptomine(ctx,*args):
       item=str(args[1])
       count=int(args[2])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (count<1):
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await miner.sellitem(ctx,str(ctx.author.id),item,count)
   elif (action=='upgrade') or (action=='up'):
     try:
       id=int(args[1])
     except:
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     if (id<1):
-      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+      await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
       return
     await miner.upgrademine(ctx,str(ctx.author.id),id)
   else:
-    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `&help`',0))
+    await io.reply(ctx,'',await builder.buildDesc('Invalid Arguments','Please use `^help`',0))
 
 bot.run(TOKEN)
