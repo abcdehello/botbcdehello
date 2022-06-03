d1f07fe37d01fa667222b174952e5fbe8ee7a22c
abcdehello <38322485+abcdehello@users.noreply.github.com>
2022-06-04 02:02:43 +0800

    added new color for embed

--- embedbuilder.py
+++ embedbuilder.py
@@ -1,17 +1,27 @@
 import discord
 
-async def buildDesc(title,description,color):
+url='https://bit.ly/3wtEnxo'
+
+async def buildDesc(title,description,color,image=None,size=0):
   embed=discord.Embed()
+  embed.url=url
+  if (size==0):
+    embed.set_thumbnail(url=image)
+  else:
+    embed.set_image(url=image)
   embed.title=title
   embed.description=description
   if (color==0):
     embed.color=discord.Color.red()
   elif (color==1):
    embed.color=discord.Color.green()
+  elif (color==2):
+    embed.color=discord.Color.yellow()
   return embed
 
 async def buildField(title,fields,color):
   embed=discord.Embed()
+  embed.url=url
   embed.title=title
   embed.description=''
   for field in fields:
@@ -20,4 +30,6 @@ async def buildField(title,fields,color):
     embed.color=discord.Color.red()
   elif (color==1):
     embed.color=discord.Color.green()
+  elif (color==2):
+    embed.color=discord.Color.yellow()
   return embed
